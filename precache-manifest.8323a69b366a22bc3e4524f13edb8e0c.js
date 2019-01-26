513e674cbb9a36b1838ff44e6027cb662c65310d
sakhile <saksman3@gmail.com>
2019-01-27 00:42:56 +0200

    Updates

---
+++ precache-manifest.8323a69b366a22bc3e4524f13edb8e0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "125486aae6a7bdbd8442",
+    "url": "/myportfolio/static/js/main.125486aa.chunk.js"
+  },
+  {
+    "revision": "a7005b8872824aa06438",
+    "url": "/myportfolio/static/js/1.a7005b88.chunk.js"
+  },
+  {
+    "revision": "125486aae6a7bdbd8442",
+    "url": "/myportfolio/static/css/main.938c6263.chunk.css"
+  },
+  {
+    "revision": "a7005b8872824aa06438",
+    "url": "/myportfolio/static/css/1.f33f73c2.chunk.css"
+  },
+  {
+    "revision": "ad5e85e0fe100645bb20912a9a37785e",
+    "url": "/myportfolio/index.html"
+  }
+];
